74e1ad5787c074ea22763a92d5ac07ef53ace370
bluexiaowei <bluexiaowei@aliyun.com>
2021-12-14 16:28:50 +0800

    feat(randomUuid.ts) 添加新的生成方法

--- randomUuid.ts
+++ randomUuid.ts
@@ -1,4 +1,8 @@
 export default function randomUuid(): string {
+  if (URL.createObjectURL) {
+    return URL.createObjectURL(new Blob()).slice(-36);
+  }
+
   return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
     const r = (Math.random() * 16) | 0;
     const v = c === "x" ? r : (r & 0x3) | 0x8;
